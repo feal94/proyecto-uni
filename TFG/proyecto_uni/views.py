cc24fea63c48b3be4ab0f1a5f11d5825b549b5ff
feal94 <alvaro.feal@udc.es>
2016-05-05 17:04:00 +0200

    Added base and index html. Slight changes to bootstrap .

--- TFG/proyecto_uni/views.py
+++ TFG/proyecto_uni/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the proyecto-uni index.")
+    return render (request, 'proyecto_uni/index.html')
